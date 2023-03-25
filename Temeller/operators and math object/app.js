18593ea4f7a6ece5772a9077fc04c179a519eddf
Emrahfidan <201805070@stu.adu.edu.tr>
2023-03-25 14:59:37 +0300

    finished math object

---
+++ Temeller/operators and math object/app.js
@@ -0,0 +1,34 @@
+let value;
+
+const number1 = 10 ,number2 = 20;
+
+value = number1 + number2;
+value = number1 - number2;
+value = number1 * number2;
+value = number1 / number2;
+value = number1 % number2;
+
+value = Math.PI;
+value = Math.E;
+
+value = Math.round(3.6); // sayıları yuvarlar
+value = Math.ceil(3.7); // yukarı yuvarlama
+value = Math.floor(3.7); // aşağı yuvarlama
+
+value = Math.sqrt(36); // karakök
+
+value = Math.abs(-10); // mutlak değer
+
+value = Math.pow(2,5); // üs alma
+
+value = Math.max(3,5,85,23); // en büyük sonucu dönecek
+value = Math.max(3,5,85,23);
+
+value = Math.random(); // rastgele sayı üretme // 0-1 arası // 1 dahil değil
+value = Math.random() * 50 ; // 0-50 arası 
+value = Math.random() * 50 + 22; // 1-62 arası
+value = Math.floor(math.random() * 50 + 22); // 1-62 arası tam sayılar
+
+
+
+console.log(value);
