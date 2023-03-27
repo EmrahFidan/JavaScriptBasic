6792199d5a9520ce7673c8d2ab39fbec33d98b19
Emrahfidan <201805070@stu.adu.edu.tr>
2023-03-27 13:36:47 +0300

    finished window object

---
+++ Temeller _2.0/window object/app.js
@@ -0,0 +1,55 @@
+console.log(this); // en genel objemiz, window objesi
+
+console.log(window);
+
+// alert
+// alert("hello");
+
+// const answer =  confirm("Emin misin ?");
+
+// console.log(answer);
+
+// if(answer){
+//     console.log("Sil gitsin");
+// }
+// else{
+//     console.log("silmeee");
+// }
+
+// const answer = prompt("2+2 = ?");
+
+// console.log(answer);
+
+// if(answer == "4"){
+//     console.log("true");
+// }
+// else{
+//     console.log("false");
+// }
+
+let value;
+
+// value = window;
+// value = window.location;
+// value = window.location.host;
+// value = window.location.port;
+// value = window.location.href;
+
+// if(confirm("sayfa yenilensin mi ?")){
+//     window.location.reload;
+// }
+// else{
+//     console.log("yenileme");
+// }
+
+// value = window.outerHeight; // pencereninin uzunluğunu verir
+// value = window.innerWidth; // pencereninin genişliğini verir içten, yani kenar çubukalrını hesaplamaz
+
+value = scrollX; // web sayfanın ne kadar scroll ettiği (yatay)
+
+
+
+
+
+
+console.log(value);
