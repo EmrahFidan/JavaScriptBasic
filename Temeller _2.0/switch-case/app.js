d303bcde38fad1dacc27a5bda7aa9655d74fbb12
Emrahfidan <201805070@stu.adu.edu.tr>
2023-03-27 12:16:47 +0300

    finished switch-case

---
+++ Temeller _2.0/switch-case/app.js
@@ -0,0 +1,8 @@
+const process = 2;
+if (process === 1) {
+  console.log("Islem 1");
+} else if (process === 2) {
+  console.log("Islem 2");
+} else if (process === 3) {
+  console.log("Islem 3");
+} else console.log("Gecersiz Islem");
